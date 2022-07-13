aa9c891b85632e28dfdb1a79767f41363a2c2159
LOFyz <leonardo_farias2010@hotmail.com>
2022-07-13 16:00:15 -0300

    chore: link to users

--- pages/index.vue
+++ pages/index.vue
@@ -9,8 +9,9 @@
         Nuxt.js course for Udemy
       </h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+        <nuxt-link to="/users">
+          <a class="button--green">Users</a>
+        </nuxt-link>
       </div>
     </div>
   </section>
